dd98eec2daf215e7392507400859d266bc5c1f67
JunLi <1206833230@qq.com>
2017-09-23 23:45:16 +0800

    2017年9月23日

--- src/cn/itcast/p/bean/Person.java
+++ src/cn/itcast/p/bean/Person.java
@@ -1,12 +1,29 @@
 package cn.itcast.p.bean;
 
-public class Person {
+public class Person extends Object{
 	private String name;
 	private int age;
 	public Person() {
 		super();
 		// TODO 自动生成的构造函数存根
 	}
+	
+	@Override
+	public int hashCode() {
+		// TODO 自动生成的方法存根
+		System.out.println(this+"...........hashCode");
+//		return name.hashCode()+age;
+		return 100;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO 自动生成的方法存根
+		System.out.println(this+"......."+obj);
+		Person p = (Person)obj;
+		return this.name.equals(p.name) && this.age == p.age;
+	}
+
 	public Person(String name, int age) {
 		super();
 		this.name = name;
@@ -24,6 +41,8 @@ public class Person {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+	public  String toString() {
+		return name + ":" + age;
+	}
 
 }
--- src/cn/itcast/p4/hashset/demo/HashSetDemo.java
+++ src/cn/itcast/p4/hashset/demo/HashSetDemo.java
@@ -8,6 +8,7 @@ public class HashSetDemo {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		HashSet hs = new HashSet();
+		String str = null;
 		hs.add("haha");
 		hs.add("xixi");
 		hs.add("hehe");
